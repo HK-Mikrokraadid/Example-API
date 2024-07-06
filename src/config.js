db4ef71a4775b0097dab32347de98107656de046
Martti Raavel <mrt@mrt.ee>
2024-07-06 15:49:44 +0300

    Test

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const config = {
   development: {
-    port: Number(process.env.PORT) || 3000,
+    port: Number(process.env.PORT),
     jwtSecret: process.env.JWT_SECRET,
-    saltRounds: Number(process.env.SALT_ROUNDS) || 10,
+    saltRounds: Number(process.env.SALT_ROUNDS),
     db: {
       host: process.env.MYSQL_HOST,
       database: process.env.MYSQL_DATABASE,
@@ -15,9 +15,9 @@ const config = {
     },
   },
   test: {
-    port: Number(process.env.PORT) || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'secret-secret',
-    saltRounds: Number(process.env.SALT_ROUNDS) || 10,
+    port: Number(process.env.PORT),
+    jwtSecret: process.env.JWT_SECRET,
+    saltRounds: Number(process.env.SALT_ROUNDS),
     db: {
       host: process.env.MYSQL_TEST_HOST,
       database: process.env.MYSQL_TEST_DATABASE,
