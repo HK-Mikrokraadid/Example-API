7f9101cc42535b8b6e092977035270bd6e415b1f
Martti Raavel <mrt@mrt.ee>
2024-10-30 19:25:12 +0200

    Update pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Build and start the test environment
-docker-compose -f docker-compose.test.yml up -d --build
+docker compose -f docker-compose.test.yml up -d --build
 
 # Run tests
-docker-compose -f docker-compose.test.yml run api_test
+docker compose -f docker-compose.test.yml run api_test
 
 # If tests pass, build and deploy production
 if [ $? -eq 0 ]; then
     echo "Tests passed. Building production..."
-    docker-compose -f docker-compose.test.yml down --remove-orphans
-    docker-compose build
-    docker-compose up -d
+    docker compose -f docker-compose.test.yml down --remove-orphans
+    docker compose build
+    docker compose up -d
 else
     echo "Tests failed. Deployment aborted."
     exit 1
